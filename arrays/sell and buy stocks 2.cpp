ea98c7b627203640178f9be793707c3ef9fa00e0
Samreet Kaur <147095554+Samreet-Sidhu@users.noreply.github.com>
2024-07-29 18:57:54 +0530

    Update sell and buy stocks 2.cpp

--- arrays/sell and buy stocks 2.cpp
+++ arrays/sell and buy stocks 2.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
      int maxProfit = 0;
+     if (prices.size()>2)
+        return 0;
+        
      for ( int i=0; i<prices.size()-1;i++){
         if (prices[i+1]>prices[i])
         maxProfit+= prices[i+1]- prices[i];
